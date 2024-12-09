f0a72def8db47fb2409d6baa40ee84ad8cbe87f6
kiwi0424 <hyc.ee11@nycu.edu.tw>
2024-12-10 01:39:22 +0800

    Lab8: Web Crawler

--- lab8/lab8.go
+++ lab8/lab8.go
@@ -1,51 +1,65 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
+	"flag"
+	"fmt"
+	"os"
 
-    "github.com/gocolly/colly"
+	"github.com/gocolly/colly"
 )
 
 func main() {
-    max := flag.Int("max", 10, "Max number of comments to show")
-    flag.Parse()
+	// 定義 flag
+	maxComments := flag.Int("max", 10, "Max number of comments to show")
+	flag.Parse()
 
-    // 檢查 flag 是否正確
-    if *max <= 0 {
-        fmt.Println("Usage of", os.Args[0])
-        flag.PrintDefaults()
-        return
-    }
+	// 檢查是否有多餘或錯誤的 flag
+	if len(flag.Args()) > 0 {
+		fmt.Println("Usage of:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
-    // 初始化 Colly 爬蟲
-    c := colly.NewCollector(
-        colly.AllowedDomains("www.ptt.cc"),
-    )
+	// 初始化 Colly Collector
+	c := colly.NewCollector()
 
-    comments := []string{}
-    var count int
+	// 用來存取留言資訊
+	type Comment struct {
+		Name    string
+		Content string
+		Time    string
+	}
+	comments := []Comment{}
 
-    // 查找並抓取留言
-    c.OnHTML(".push", func(e *colly.HTMLElement) {
-        if count >= *max {
-            return
-        }
-        id := e.ChildText(".push-userid")
-        content := e.ChildText(".push-content")
-        time := e.ChildText(".push-ipdatetime")
+	// 抓取留言的選擇器
+	c.OnHTML("div.push", func(e *colly.HTMLElement) {
+		name := e.ChildText("span.push-userid")
+		content := e.ChildText("span.push-content")
+		time := e.ChildText("span.push-ipdatetime")
+		if name != "" && content != "" && time != "" {
+			// 去掉留言前的冒號和空白
+			trimmedContent := content[2:]
+			comments = append(comments, Comment{
+				Name:    name,
+				Content: trimmedContent,
+				Time:    time,
+			})
+		}
+	})
 
-        comment := fmt.Sprintf("名字：%s，留言%s，時間： %s", id, content, time)
-        comments = append(comments, comment)
-        count++
-    })
+	// 訪問指定的 PTT 網址
+	url := "https://www.ptt.cc/bbs/joke/M.1481217639.A.4DF.html"
+	err := c.Visit(url)
+	if err != nil {
+		fmt.Println("Error visiting URL:", err)
+		os.Exit(1)
+	}
 
-    // 爬取指定頁面
-    c.Visit("https://www.ptt.cc/bbs/joke/M.1481217639.A.4DF.html")
-
-    // 輸出抓取到的留言
-    for i, comment := range comments {
-        fmt.Printf("%d. %s\n", i+1, comment)
-    }
+	// 印出留言資訊，最多 *maxComments 筆
+	for i, comment := range comments {
+		if i >= *maxComments {
+			break
+		}
+		fmt.Printf("%d. 名字：%s，留言: %s，時間： %s\n", i+1, comment.Name, comment.Content, comment.Time)
+	}
 }
